5da67056bef82710dec84ac8c3adea5b4434f724
Camilo Arriagada Vallejos <95378920+c4miloarriagada@users.noreply.github.com>
2022-07-27 19:57:47 -0400

    after-prod

--- api/models/server.js
+++ api/models/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 class Server{
     constructor(){
         this.app = express();
-        this.port = process.env.PORT || 8000;
+        this.port = process.env.PORT;
         this.path = {
             iecho : '/iecho'
         }
